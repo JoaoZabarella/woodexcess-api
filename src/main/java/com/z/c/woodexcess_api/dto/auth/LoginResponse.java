2e1c934b44f340638ece50428c483538be009528
João Pedro Zabarella <zabarellaj@gmail.com>
2025-11-23 18:51:52 -0300

    feat: Add initial Spring Boot configuration files and update Docker database port mapping to 5438.

--- src/main/java/com/z/c/woodexcess_api/dto/auth/LoginResponse.java
+++ src/main/java/com/z/c/woodexcess_api/dto/auth/LoginResponse.java
@@ -1,15 +1,13 @@
 package com.z.c.woodexcess_api.dto.auth;
 
-import com.z.c.woodexcess_api.role.UserRole;
-
 public record LoginResponse(
         String accessToken,
         String refreshToken,
         String tokenType,
         Long expiresIn
 
-){
-    public LoginResponse(String accessToken, String refreshToken, Long expiresIn){
+) {
+    public LoginResponse(String accessToken, String refreshToken, Long expiresIn) {
         this(accessToken, refreshToken, "Bearer", expiresIn);
     }
 }
