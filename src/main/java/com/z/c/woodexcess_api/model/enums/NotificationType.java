78e24fec90b8cffee752e132106716553c9bb446
João Pedro Zabarella <zabarellaj@gmail.com>
2025-12-15 21:34:35 -0300

    feat(model): add offer domain enums

---
+++ src/main/java/com/z/c/woodexcess_api/model/enums/NotificationType.java
@@ -0,0 +1,10 @@
+package com.z.c.woodexcess_api.model.enums;
+
+public enum NotificationType {
+    NEW_OFFER,
+    OFFER_ACCEPTED,
+    OFFER_REJECTED,
+    COUNTER_OFFER,
+    OFFER_EXPIRED,
+    OFFER_CANCELLED
+}
